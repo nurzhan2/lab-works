dbf6effac844c775d7a25e012ebc0256335fa3b5
nurzhan <kalmuratnurzhan@gmail.com>
2025-02-04 11:38:29 +0500

    lab2

--- lab2.py
+++ lab2.py
@@ -1,450 +1,126 @@
-#bolleans
-
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+# Booleans
+print("Boolean Comparisons:")
+print(10 > 9)       # True
+print(10 == 9)      # False
+print(10 < 9)       # False
 
 a = 200
 b = 33
-
+print("\nBoolean in Conditional:")
 if b > a:
-  print("b is greater than a")
+    print("b is greater than a")
 else:
-  print("b is not greater than a")
+    print("b is not greater than a")
 
-print(bool("Hello"))
-print(bool(15))
+print("\nBoolean Values:")
+print(bool("Hello"))    # True
+print(bool(15))         # True
+print(bool([]))         # False
+print(bool(0))          # False
 
-x = "Hello"
-y = 15
+class MyClass:
+    def __len__(self):
+        return 0
 
-print(bool(x))
-print(bool(y))
+myobj = MyClass()
+print("\nClass Boolean:")
+print(bool(myobj))      # False
 
-bool("abc")
-bool(123)
-bool(["apple", "cherry", "banana"])
+def my_function():
+    return True
 
-bool(False)
-bool(None)
-bool(0)
-bool("")
-bool(())
-bool([])
-bool({})
+print("\nFunction Return:")
+print(my_function())    # True
 
-class myclass():
-  def __len__(self):
-    return 0
-
-myobj = myclass()
-print(bool(myobj))
-
-def myFunction() :
-  return True
-
-print(myFunction())
-
-def myFunction() :
-  return True
-
-if myFunction():
-  print("YES!")
+if my_function():
+    print("YES!")       # YES!
 else:
-  print("NO!")
+    print("NO!")
 
+print("\nType Check:")
 x = 200
-print(isinstance(x, int))
+print(isinstance(x, int))  # True
 
-#Operators
+# Operators
+print("\nOperators:")
+print(10 + 5)           # 15
+print((6 + 3) - (6 + 3)) # 0
+print(100 + 5 * 3)      # 115
 
-print(10 + 5)
+# Lists
+print("\nList Operations:")
+fruits = ["apple", "banana", "cherry"]
+print("Original List:", fruits)
+print("Length:", len(fruits))
 
-print((6 + 3) - (6 + 3))
+fruits[1] = "blackcurrant"
+print("Modified List:", fruits)
 
-print(100 + 5 * 3)
+fruits.append("orange")
+print("After Append:", fruits)
 
-#Lists
+fruits.insert(1, "mango")
+print("After Insert:", fruits)
 
-mylist = ["apple", "banana", "cherry"]
+fruits.remove("blackcurrant")
+print("After Remove:", fruits)
 
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-print(len(thislist))
-
-list1 = ["apple", "banana", "cherry"]
-list2 = [1, 5, 7, 9, 3]
-list3 = [True, False, False]
-
-list1 = ["abc", 34, True, 40, "male"]
-
-mylist = ["apple", "banana", "cherry"]
-print(type(mylist))
-
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
-print(thislist)
-
-#Loop Lists
-
-thislist = ["apple", "banana", "cherry"]
-for x in thislist:
-  print(x)
-
-thislist = ["apple", "banana", "cherry"]
-for i in range(len(thislist)):
-  print(thislist[i])
-
-thislist = ["apple", "banana", "cherry"]
-i = 0
-while i < len(thislist):
-  print(thislist[i])
-  i = i + 1
-
-thislist = ["apple", "banana", "cherry"]
-[print(x) for x in thislist]
-
-#Access List Items
-
-thislist = ["apple", "banana", "cherry"]
-print(thislist[1])
-
-thislist = ["apple", "banana", "cherry"]
-print(thislist[-1])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[:4])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[-4:-1])
-
-thislist = ["apple", "banana", "cherry"]
-if "apple" in thislist:
-  print("Yes, 'apple' is in the fruits list")
-
-#Change List Items
-
-thislist = ["apple", "banana", "cherry"]
-thislist[1] = "blackcurrant"
-print(thislist)
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["blackcurrant", "watermelon"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist[1:2] = ["blackcurrant", "watermelon"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist[1:3] = ["watermelon"]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(2, "watermelon")
-print(thislist)
-
-#Add List Items
-
-thislist = ["apple", "banana", "cherry"]
-thislist.append("orange")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.insert(1, "orange")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-tropical = ["mango", "pineapple", "papaya"]
-thislist.extend(tropical)
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thistuple = ("kiwi", "orange")
-thislist.extend(thistuple)
-print(thislist)
-
-#Remove List Items
-
-thislist = ["apple", "banana", "cherry"]
-thislist.remove("banana")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
-thislist.remove("banana")
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.pop(1)
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-thislist.pop()
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-del thislist[0]
-print(thislist)
-
-thislist = ["apple", "banana", "cherry"]
-del thislist
-
-thislist = ["apple", "banana", "cherry"]
-thislist.clear()
-print(thislist)
-
-#List Comprehension
-
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
-
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
+print("\nList Comprehension:")
+newlist = [x.upper() for x in fruits if "a" in x]
 print(newlist)
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+fruits.sort()
+print("\nSorted List:", fruits)
 
-newlist = [x for x in fruits if "a" in x]
+# Tuples
+print("\nTuple Operations:")
+thistuple = ("apple", "banana", "cherry")
+print("Original Tuple:", thistuple)
+print("Tuple Length:", len(thistuple))
 
-print(newlist)
+print("\nTuple Unpacking:")
+(green, yellow, red) = thistuple
+print(green, yellow, red)
 
-newlist = [x for x in fruits if x != "apple"]
+# Sets
+print("\nSet Operations:")
+thisset = {"apple", "banana", "cherry"}
+print("Original Set:", thisset)
+thisset.add("orange")
+print("After Add:", thisset)
 
-newlist = [x for x in range(10) if x < 5]
+# Dictionaries
+print("\nDictionary Operations:")
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print("Original Dictionary:", car)
+print("Dictionary Keys:", car.keys())
 
-newlist = [x.upper() for x in fruits]
+car["year"] = 2020
+print("\nUpdated Dictionary:", car)
 
-newlist = [x if x != "banana" else "orange" for x in fruits]
+# Control Flow
+print("\nControl Flow:")
+for i in range(5):
+    if i == 3:
+        continue
+    print(i, end=' ')
+print()
 
-#sort list
+# Error Handling Example (commented out)
+# thistuple = ("apple", "banana", "cherry")
+# del thistuple
+# print(thistuple)  # This will raise an error
 
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
-print(thislist)
+print("\nLoop Through Dictionary:")
+for key, value in car.items():
+    print(f"{key}: {value}")
 
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(reverse = True)
-print(thislist)
-
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.reverse()
-print(thislist)
-
-#copy list
-
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
-
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
-
-#Join Lists
-
+print("\nFinal Combined List:")
 list1 = ["a", "b", "c"]
 list2 = [1, 2, 3]
-
 list3 = list1 + list2
 print(list3)
-
-list1 = ["a", "b" , "c"]
-list2 = [1, 2, 3]
-
-for x in list2:
-  list1.append(x)
-
-print(list1)
-
-list1 = ["a", "b" , "c"]
-list2 = [1, 2, 3]
-
-list1.extend(list2)
-print(list1)
-
-
-#Tuples
-
-thistuple = ("apple", "banana", "cherry")
-print(thistuple)
-
-thistuple = ("apple", "banana", "cherry")
-print(len(thistuple))
-
-tuple1 = ("abc", 34, True, 40, "male")
-
-#Access Tuple Items
-
-thistuple = ("apple", "banana", "cherry")
-print(thistuple[1])
-print(thistuple[-1])
-print(thistuple[1:2])
-print(thistuple[:2])
-print(thistuple[1:])
-
-#update tuples 
-
-x = ("apple", "banana", "cherry")
-y = list(x)
-y[1] = "kiwi"
-x = tuple(y)
-
-print(x)
-
-thistuple = ("apple", "banana", "cherry")
-y = list(thistuple)
-y.append("orange")
-thistuple = tuple(y)
-
-thistuple = ("apple", "banana", "cherry")
-y = ("orange",)
-thistuple += y
-
-print(thistuple)
-
-thistuple = ("apple", "banana", "cherry")
-del thistuple
-print(thistuple)
-
-#Unpack Tuples
-
-fruits = ("apple", "banana", "cherry")
-
-(green, yellow, red) = fruits
-
-print(green)
-print(yellow)
-print(red)
-
-# Loop Tuples
-
-thistuple = ("apple", "banana", "cherry")
-for x in thistuple:
-  print(x)
-
-thistuple = ("apple", "banana", "cherry")
-for i in range(len(thistuple)):
-  print(thistuple[i])
-
-thistuple = ("apple", "banana", "cherry")
-i = 0
-while i < len(thistuple):
-  print(thistuple[i])
-  i = i + 1
-
-#join tuples
-
-tuple1 = ("a", "b" , "c")
-tuple2 = (1, 2, 3)
-
-tuple3 = tuple1 + tuple2
-print(tuple3)
-
-fruits = ("apple", "banana", "cherry")
-mytuple = fruits * 2
-
-print(mytuple)
-
-#Sets
-
-thisset = {"apple", "banana", "cherry"}
-print(thisset)
-
-print(len(thisset))
-
-print(type(thisset))
-
-#True and 1 is considered the same value
-
-thisset = set(("apple", "banana", "cherry")) 
-print(thisset)
-
-#Dictionary
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict)
-print(thisdict["brand"])
-
-#Duplicates Not Allowed
-
-print(len(thisdict))
-
-print(type(thisdict))
-
-thisdict = dict(name = "John", age = 36, country = "Norway")
-print(thisdict)
-
-#Accessing Items
-
-x = thisdict.keys()
-
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-
-x = car.keys()
-
-print(x) #before the change
-
-car["color"] = "white"
-
-print(x) #after the change
-
-#update 
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.update({"year": 2020})
-
-#remove
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.pop("model")
-thisdict.popitem()
-print(thisdict)
-
-#if else
-
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
-
-#while loops
-
-# break = stop  iteration 
-# continue = stop loop 
-
-# for loops
-
-# for "item" in range (start , end , step):
-#   "code" 
